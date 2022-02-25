6b850449de483b23300caaad207b5ddb2f62d3f3
Florian Salaun <florian.salaun@nokia.com>
2022-02-25 13:59:40 +0100

    Corrected issue and added clicking

--- main.py
+++ main.py
@@ -35,13 +35,24 @@ def random_init(board, canvas):
     update_canvas(board, canvas)
 
 
+def clear_init_board(board):
+    for row in board:
+        for cell in row:
+            cell.is_alive = False
+
+
+def clear_init(board, canvas):
+    clear_init_board(board)
+    update_canvas(board, canvas)
+
+
 def compute_next_board(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             number_of_neighbors = 0
             for x in range(i - 1, i + 2):
                 for y in range(j - 1, j + 2):
-                    if x in range(len(board)) and y in range(len(board)) \
+                    if x in range(len(board)) and y in range(len(board[x])) \
                             and (x != i or y != j) and board[x][y].is_alive:
                         number_of_neighbors += 1
             if number_of_neighbors <= 1 or number_of_neighbors >= 4:
@@ -83,16 +94,27 @@ def stop_button():
     stop_loop = True
 
 
+def button_pressed(event, board, canvas):
+    size_x = size_y = 40
+    i = event.x // size_x
+    j = event.y // size_x
+    board[i][j].is_alive = not board[i][j].is_alive
+    update_canvas(board, canvas)
+
+
 if __name__ == "__main__":
     board = [[Cell() for i in range(20)] for j in range(20)]
     top = tk.Tk()
     top.title("Game Of Life")
-    c = Canvas(top, bg="grey", height=800, width=800)
+    c = Canvas(top, bg="black", height=800, width=800)
+    c.bind("<Button-1>", lambda event: button_pressed(event, board, c))
     c.pack()
+    init_button = tk.Button(top, text="New Random Initialization", command=lambda: random_init(board, c))
+    init_button.pack()
+    clear_button = tk.Button(top, text="New Empty Initialization", command=lambda: clear_init(board, c))
+    clear_button.pack()
     next_button = tk.Button(top, text="Next Iteration", command=lambda: next_iteration(board, c))
     next_button.pack()
-    init_button = tk.Button(top, text="New Initialization", command=lambda: random_init(board, c))
-    init_button.pack()
     auto_button = tk.Button(top, text="Auto Iteration", command=lambda: auto_iteration(board, c, 0))
     auto_button.pack()
     stop_button = tk.Button(top, text="Stop Auto Iteration", command=stop_button)
